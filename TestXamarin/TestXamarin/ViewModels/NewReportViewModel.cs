cdf69b524797c32883d84030d3c774c33cffbb2f
aleksjjj11 <aleksjjj11@fex.net>
2020-07-31 13:37:37 +0500

    Init NewReportPage & NewReportViewModel

---
+++ TestXamarin/TestXamarin/ViewModels/NewReportViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestXamarin.Models;
+using Xamarin.Forms;
+
+namespace TestXamarin.ViewModels
+{
+    public class NewReportViewModel : BaseViewModel
+    {
+        public Report report;
+        public NewReportViewModel()
+        {
+            Title = "Новая жалоба";
+            report = new Report();
+        }
+    }
+}
---
+++ TestXamarin/TestXamarin/Views/NewReportPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestXamarin.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestXamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewReportPage : ContentPage
+    {
+        NewReportViewModel viewModel;
+        public NewReportPage()
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new NewReportViewModel();
+        }
+
+    }
+}
